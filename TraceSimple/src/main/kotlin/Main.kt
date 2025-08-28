43fbefcb8031b1a476f5920edcc9c3cfb85c9c6e
Charles Boucher <2474516@cegepmontpetit.ca>
2025-08-28 09:40:41 -0400

    Trace d'exécution
    Produire une trace d'exécution

---
+++ TraceSimple/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+package org.Boucher
+//Main
+// val monNombre = .1 + .1 + .1 = 0.3
+// var maChaine = "Bonjour"
+//maChaine += " le monde"
+//maChaine == "Bonjour le monde"
+// println("monNombre = $monNombre") affiche 0.3
+// println("maChaine = $maChaine") affiche Bonjour le monde
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val monNombre = .1 + .1 + .1
+    var maChaine = "Bonjour"
+    maChaine += " le monde"
+    println("monNombre = $monNombre")
+    println("maChaine = $maChaine")
+}
